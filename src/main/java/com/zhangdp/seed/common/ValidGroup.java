803f7801ddad1e59381485f607084845ffd206eb
zhangdp <898103692@qq.com>
2023-04-04 14:53:04 +0800

    first commit

---
+++ src/main/java/com/zhangdp/seed/common/ValidGroup.java
@@ -0,0 +1,31 @@
+package com.zhangdp.seed.common;
+
+/**
+ * 校验类型分组
+ *
+ * @author zhangdp
+ * @since 1.0.0
+ */
+public class ValidGroup {
+
+    /**
+     * 插入组
+     *
+     * @author zhangdp
+     * @since 1.0.0
+     */
+    public static interface Insert {
+
+    }
+
+    /**
+     * 修改组
+     *
+     * @author zhangdp
+     * @since 1.0.0
+     */
+    public static interface Update {
+
+    }
+
+}
