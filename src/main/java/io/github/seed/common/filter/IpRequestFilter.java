dd303b3a0a0dea96b652e85b0d09cfa7ce74573a
zhangdp <898103692@qq.com>
2025-04-18 10:17:23 +0800

    first commit

---
+++ src/main/java/io/github/seed/common/filter/IpRequestFilter.java
@@ -0,0 +1,69 @@
+package io.github.seed.common.filter;
+
+import io.github.seed.common.enums.ErrorCode;
+import io.github.seed.common.exception.ForbiddenException;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.dromara.hutool.core.text.StrUtil;
+import org.dromara.hutool.http.server.servlet.ServletUtil;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * ip过滤器
+ *
+ * @author zhangdp
+ * @since 2025/3/14
+ */
+@Slf4j
+public class IpRequestFilter extends OncePerRequestFilter {
+
+    /**
+     * ip列表
+     */
+    private final Set<String> ipSet;
+    /**
+     * 过滤模式，默认白名单
+     */
+    private final FilterMode filterMode;
+
+    public IpRequestFilter(Collection<String> ipList, FilterMode filterMode) {
+        this.ipSet = new HashSet<>(ipList);
+        this.filterMode = filterMode;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (log.isTraceEnabled()) {
+            log.trace("IP Request Filter in. uri={}, ipSet={}, filterMode={}", request.getRequestURI(), ipSet, filterMode);
+        }
+        String ip = ServletUtil.getClientIP(request);
+        boolean flag = StrUtil.isNotBlank(ip) && ipSet.contains(ip);
+        // ip不在白名单里面时不允许访问
+        if (filterMode == FilterMode.WHITE_LIST && !flag) {
+            throw new ForbiddenException(ErrorCode.IP_NOT_ALLOW);
+        }
+        // ip在黑名单里时不允许访问
+        if (filterMode == FilterMode.BLACK_LIST && flag) {
+            throw new ForbiddenException(ErrorCode.IP_NOT_ALLOW);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    /**
+     * 过滤模式枚举
+     */
+    public enum FilterMode {
+        // 黑名单
+        WHITE_LIST,
+        // 白名单
+        BLACK_LIST
+    }
+
+}
